6b47661ccef0dc9f32c3d2a5b270abad6233dbb1
Shreyas <shreoriginal@gmail.com>
2026-08-29 14:44:42 +0530

    Add arrays assignment problems: TwoSum, MaxProfit, ContainsDuplicate, MergeSortedArrays, RotateArray

---
+++ src/main/java/arrays/assigment_problems/MaxProfit.java
@@ -0,0 +1,22 @@
+package arrays.assigment_problems;
+
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int bestProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > bestProfit) {
+                bestProfit = price - minPrice;
+            }
+        }
+        return bestProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
---
+++ src/main/java/arrays/assigment_problems/RotateArray.java
@@ -0,0 +1,21 @@
+package arrays.assigment_problems;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static int[] rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] newArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            newArray[(i + k) % n] = nums[i];
+        }
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println(Arrays.toString(rotateArray(new int[]{1, 2}, 3)));
+    }
+}
